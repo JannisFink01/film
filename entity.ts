fb42b75c278f723075dca5328d0092eb7ab7716e
Valentin Moritz <vali.moritz@gmail.com>
2023-04-17 22:20:26 +0200

    entity class

---
+++ entity.ts
@@ -0,0 +1,55 @@
+import {
+    Column,
+    CreateDateColumn,
+    Entity,
+    OneToMany,
+    OneToOne,
+    PrimaryGeneratedColumn,
+    UpdateDateColumn,
+    VersionColumn,
+} from 'typeorm';
+
+
+
+@Entity()
+export class Film {
+    @PrimaryGeneratedColumn()
+    id: number | undefined;
+
+    @VersionColumn()
+    readonly version: number | undefined;
+
+    @Column('int')
+    @ApiProperty({ example: 5, type: Number })
+    readonly bewertung: number | undefined;
+
+    @Column('varchar', { length: 12 })
+    @ApiProperty({ example: 'CD', type: String })
+    readonly art: FilmArt | undefined;
+
+    @Column('decimal', {
+        precision: 8,
+        scale: 2,
+        transformer: new DecimalTransformer(),
+    })
+    @ApiProperty({ example: 1, type: Number })
+    readonly preis!: number;
+
+
+    @Column('boolean')
+    @ApiProperty({ example: true, type: Boolean })
+    readonly ausleihbar: boolean | undefined;
+
+    @Column('date')
+    @ApiProperty({ example: '2023-02-28' })
+    readonly erscheinungsdatum: Date | string | undefined;
+
+    @Column('simple-array')
+    readonly beschreibung: string[] | undefined;
+
+    @OneToOne(() => Titel, (titel) => titel.film, {
+        cascade: ['insert', 'remove'],
+    })
+    readonly titel: Titel | undefined;
+
+}
