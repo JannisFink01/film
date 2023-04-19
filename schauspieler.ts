1ce62885028aa675dd05752b9a5a3d011a521ffc
Vvalentin <118907208+Vvalentin@users.noreply.github.com>
2023-04-19 14:58:09 +0200

    Add files via upload

---
+++ schauspieler.ts
@@ -0,0 +1,20 @@
+import {
+    Column,
+    Entity,
+    JoinColumn,
+    ManyToOne,
+    PrimaryGeneratedColumn,
+} from 'typeorm';
+import { Film } from './film.js';
+
+@Entity()
+export class Schauspieler {
+    @Column('int')
+    @PrimaryGeneratedColumn()
+    id: number | undefined;
+
+    @Column('varchar', { unique: true, length: 32 })
+    readonly beschriftung!: string;
+}
+
+
---
+++ titel.ts
@@ -0,0 +1,18 @@
+import {
+    Column,
+    Entity,
+    JoinColumn,
+    OneToOne,
+    PrimaryGeneratedColumn,
+} from 'typeorm';
+
+
+@Entity()
+export class Titel {
+    @Column('int')
+    @PrimaryGeneratedColumn()
+    id: number | undefined;
+
+    @Column('varchar', { unique: true, length: 40 })
+    readonly titel!: string;
+}
