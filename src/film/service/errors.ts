6c9c6d538ca25fd4a5a10683ebb2ead56dabe9b9
bichanhoang <bichanhoang00@gmail.com>
2023-04-27 17:58:26 +0200

    added mail, errors and write service

---
+++ src/film/service/errors.ts
@@ -0,0 +1,39 @@
+/**
+ * Das Modul besteht aus den Klassen für die Fehlerbehandlung bei der Verwaltung
+ * von Filmen, z.B. beim DB-Zugriff.
+ * @packageDocumentation
+ */
+
+/**
+ * Klasse für eine ungültige Versionsnummer beim Ändern.
+ */
+export interface VersionInvalid {
+    readonly type: 'VersionInvalid';
+    readonly version: string | undefined;
+}
+
+/**
+ * Klasse für eine veraltete Versionsnummer beim Ändern.
+ */
+export interface VersionOutdated {
+    readonly type: 'VersionOutdated';
+    readonly id: number;
+    readonly version: number;
+}
+
+/**
+ * Klasse für einen nicht-vorhandenen Film beim Ändern.
+ */
+export interface FilmNotExists {
+    readonly type: 'FilmNotExists';
+    readonly id: number | undefined;
+}
+
+/**
+ * Union-Type für Fehler beim Ändern eines Filmes:
+ * - {@linkcode FilmNotExists}
+ * - {@linkcode ConstraintViolations}
+ * - {@linkcode VersionInvalid}
+ * - {@linkcode VersionOutdated}
+ */
+export type UpdateError = FilmNotExists | VersionInvalid | VersionOutdated;
