be7c06d2710034973c5c44f9990fa79e9890ba5b
bichanhoang <bichanhoang00@gmail.com>
2023-05-09 14:31:44 +0200

    fixed sql scripts for create and drop tables

--- src/config/dev/postgres/create.sql
+++ src/config/dev/postgres/create.sql
@@ -24,10 +24,6 @@ CREATE SCHEMA IF NOT EXISTS AUTHORIZATION film;
 
 ALTER ROLE film SET search_path = 'film';
 
--- https://www.postgresql.org/docs/current/sql-createtype.html
--- https://www.postgresql.org/docs/current/datatype-enum.html
-CREATE TYPE filmart AS ENUM ('DRUCKAUSGABE', 'KINDLE');
-
 -- https://www.postgresql.org/docs/current/sql-createtable.html
 -- https://www.postgresql.org/docs/current/datatype.html
 CREATE TABLE IF NOT EXISTS film (
@@ -39,21 +35,15 @@ CREATE TABLE IF NOT EXISTS film (
     version       integer NOT NULL DEFAULT 0,
                   -- impliziter Index als B-Baum durch UNIQUE
                   -- https://www.postgresql.org/docs/current/ddl-constraints.html#DDL-CONSTRAINTS-UNIQUE-CONSTRAINTS
-    isbn          varchar(17) NOT NULL UNIQUE USING INDEX TABLESPACE filmspace,
+    regisseur          varchar(40) NOT NULL USING INDEX TABLESPACE filmspace,
                   -- https://www.postgresql.org/docs/current/ddl-constraints.html#DDL-CONSTRAINTS-CHECK-CONSTRAINTS
                   -- https://www.postgresql.org/docs/current/functions-matching.html#FUNCTIONS-POSIX-REGEXP
-    rating        integer NOT NULL CHECK (rating >= 0 AND rating <= 5),
-    art           filmart,
+    bewertung        integer NOT NULL CHECK (rating >= 0 AND rating <= 5),
                   -- https://www.postgresql.org/docs/current/datatype-numeric.html#DATATYPE-NUMERIC-DECIMAL
                   -- 10 Stellen, davon 2 Nachkommastellen
     preis         decimal(8,2) NOT NULL,
-    rabatt        decimal(4,3) NOT NULL,
-                  -- https://www.postgresql.org/docs/current/datatype-boolean.html
-    lieferbar     boolean NOT NULL DEFAULT FALSE,
                   -- https://www.postgresql.org/docs/current/datatype-datetime.html
-    datum         date,
-    homepage      varchar(40),
-    schlagwoerter varchar(64),
+    erscheiungsdatum         date,
                   -- https://www.postgresql.org/docs/current/datatype-datetime.html
     erzeugt       timestamp NOT NULL DEFAULT NOW(),
     aktualisiert  timestamp NOT NULL DEFAULT NOW()
@@ -62,15 +52,13 @@ CREATE TABLE IF NOT EXISTS film (
 CREATE TABLE IF NOT EXISTS titel (
     id          integer GENERATED ALWAYS AS IDENTITY(START WITH 1000) PRIMARY KEY USING INDEX TABLESPACE filmspace,
     titel       varchar(40) NOT NULL,
-    untertitel  varchar(40),
     film_id     integer NOT NULL UNIQUE USING INDEX TABLESPACE filmspace REFERENCES film
 ) TABLESPACE filmspace;
 
 
-CREATE TABLE IF NOT EXISTS abbildung (
+CREATE TABLE IF NOT EXISTS schauspieler (
     id              integer GENERATED ALWAYS AS IDENTITY(START WITH 1000) PRIMARY KEY USING INDEX TABLESPACE filmspace,
-    beschriftung    varchar(32) NOT NULL,
-    content_type    varchar(16) NOT NULL,
+    name    varchar(40) NOT NULL,
     film_id         integer NOT NULL REFERENCES film
 ) TABLESPACE filmspace;
-CREATE INDEX IF NOT EXISTS abbildung_film_id_idx ON abbildung(film_id) TABLESPACE filmspace;
+CREATE INDEX IF NOT EXISTS schauspieler_film_id_idx ON schauspieler(film_id) TABLESPACE filmspace;
